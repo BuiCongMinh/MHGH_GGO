33128ad42d9abf352299370d14879ef0c0e5849d
Test_Terminal_git <minhvnvnvn1998@gmail.com>
2024-04-10 12:51:00 +0700

    hoan thanh main page

---
+++ Main_page/mainPage.js
@@ -0,0 +1,57 @@
+const goTopBtn = document.querySelector(".go-top-btn");
+const reload = document.querySelector(".reload");
+const sideBar = document.querySelector(".sideBar .menuApp");
+const itemTitles = document.querySelectorAll(".item_title");
+const contents = document.querySelectorAll(".content-body");
+
+window.addEventListener("scroll", checkHeight);
+
+function checkHeight() {
+  if (window.scrollY > 400) {
+    goTopBtn.style.transform = "translateY(0px) translateX(-50%)";
+
+    sideBar.style.transform = "translateY(50%)";
+    sideBar.style.backgroundPosition = "-186px -1014px";
+    sideBar.classList.add("show");
+  } else {
+    sideBar.style.transform = "translateY(50%) translateX(150px)";
+    sideBar.style.backgroundPosition = "0 -1014px";
+    sideBar.classList.remove("show");
+
+    goTopBtn.style.transform = "translateY(100px) translateX(-50%)";
+  }
+}
+
+sideBar.addEventListener("click", () => {
+  sideBar.classList.toggle("show");
+  if (!sideBar.classList.contains("show")) {
+    sideBar.style.transform = "translateY(50%) translateX(150px)";
+    sideBar.style.backgroundPosition = "0 -1014px";
+  } else {
+    sideBar.style.transform = "translateY(50%)";
+    sideBar.style.backgroundPosition = "-186px -1014px";
+  }
+});
+
+goTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+reload.addEventListener("click", () => {
+  window.location.reload();
+});
+
+itemTitles.forEach((itemTitle, index) => {
+  itemTitle.addEventListener("click", () => {
+    document.querySelector(".sector2  .right .title .active")?.classList.remove("active");
+    itemTitle.classList.add("active");
+  
+    document.querySelector('.sector2 .content .active')?.classList.remove("active");
+    contents[index].classList.add("active");
+    
+    });
+});
+
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+Cần tải font awsome vào để có icon anh ko add được font awsome lên git được 
+
+sau đó rồi chạy bình thường :)))
